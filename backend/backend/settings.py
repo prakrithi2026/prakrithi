db438c390ad131b9fe98751b6b5d4da2f10f90b6
prakrithi2026 <sale.prakrithi@gmail.com>
2026-07-25 12:08:50 +0530

    Update default database config to use DATABASE_URL env var

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -91,9 +91,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:govind@271@localhost:5432/prakrithi_db',
-        conn_max_age=600
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
     )
 }
 
